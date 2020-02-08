5fbc308025becfb8f383a94a3da5ae6a831ad25a
Arpit Tyagi <51491402+arpittyagi29@users.noreply.github.com>
2020-02-08 13:33:48 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+let result=0;
+
+function getuserinput(){
+    return parseInt(userInput.value)
+}
+function add(){
+    const getnum=getuserinput();
+    const desp=`${result}+${getnum}`;
+     result=result + getnum;
+    outputResult(result,desp);
+}
+
+function subtract(){
+     const getnum1=getuserinput();
+    const desp=`${result}-${getnum1}`;
+     result=result - getnum1;
+    outputResult(result,desp);
+    
+}
+function multiplication(){
+    const getnum2=getuserinput();
+    const desp=`${result}*${getnum2}`;
+     result=result * getnum2;
+    outputResult(result,desp);
+    
+}
+function divide(){
+    const getnum3=getuserinput();
+    const desp=`${result}/${getnum3}`;
+     result=result / getnum3;
+    outputResult(result,desp);
+    
+}
+addBtn.addEventListener('click',add);
+subtractBtn.addEventListener('click',subtract);
+multiplyBtn.addEventListener('click',multiplication);
+divideBtn.addEventListener('click',divide);
